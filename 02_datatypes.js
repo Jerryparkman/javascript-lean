965a4867287fb989cfc03df35f6088f46c903c01
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-26 23:43:08 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"
+//number
+// bigInt
+// string
+// boolean
+// objects
+// undefined
+// null
+
+let x="hello12";
+console.log(typeof x);
+
+console.log("HELLO".toLowerCase());
+console.log("hey buddy its me satrohan sharma and i'm from kantibazar and i'm a computer engineering student!".toUpperCase());
+console.log("HELLO DEAR, ONE THINK THAT I WANT TO TELL YOU THAT I'M AFFRAID TO TELL YOU THAT!".toLowerCase());
