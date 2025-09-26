9bf7fa9f33da85542e3b09466c4b0f8e987c0279
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-26 23:47:34 +0000

    Added new code lines

--- first.js
+++ first.js
@@ -1,1 +1,29 @@
 console.log("hello".concat(" ","world"));
+const name= "satrohan";
+let number= 9807827509;
+var email= "satrohan@gmail.com";
+const address = ""; 
+let A = Number(address);
+console.log (A);
+
+
+
+function name1(a, b, c, d){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+}
+
+name1(1,2,3,4);
+
+let preeti = {
+    name: "preeti chaudhari",
+    address: 'gaushala',
+    relation: "girlfriend",
+    from: "6month",
+    age: 21,
+    education: "bachlor running"
+}
+console.log(preeti);
+
