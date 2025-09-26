35f52c69696ae70cda9c5040794cba23ce89a746
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-26 23:48:19 +0000

    shortcuts

---
+++ 00.shortcuts.js
@@ -0,0 +1,7 @@
+console.log("HELLO".charAt(3));
+"hello".charAt(3);
+
+// "".toUpperCase(). IT used for making anything in upper letter
+// "".toLowerCase(). It used for making anything in lower letter
+// "hello".chatAt(3). To find the position of the string Eg. it gives the output (L)
+
