0d503154327c06eba2747e6e506a04096d0aa73b
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-26 23:40:48 +0000

    variables

---
+++ 01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 12345;
+let accountEmail = "Satrohan@gmail.com";
+var accountPassword = "satrohan23";
+
+accountPassword = "hello buddy";
+
+
+let Student ={
+    name: "satrohan",
+    age: 20,
+    address: "mahottari",
+    phone: 9807827509,
+    email: "satrohan"
+};
+
+function name(id){
+    console.log(id);
+}
+
+name(5);
+
+for(i=0; i<=5; i++){
+    // console.log("javascript");
+}
+let j=1;
+while(j<=6){
+    console.log("i love javascript");
+    j++;
+}
