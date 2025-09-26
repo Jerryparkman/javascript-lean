92edca46daa80cef2c580a3a44ce8a648020f257
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-26 23:46:42 +0000

    comparison

---
+++ 04_comparisonOfDataTypes.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 !=1 );
+  
+// console.log("2" > 1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >=0);
+// console.log(null <= 0);
+
+// undefined
+console.log(undefined == 0 );
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined != 0);
+
+// strict check 
+console.log("2" === 2);
+// it does not check only the value it also check the data type of the value which is given and used 
