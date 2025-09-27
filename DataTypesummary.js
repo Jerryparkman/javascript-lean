aee0f587b28794fba9e5b87145f57aaaa4f456f2
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-27 00:42:37 +0000

    datatypes

---
+++ DataTypesummary.js
@@ -0,0 +1,25 @@
+// data types 
+// Premitive and Non-Premitive
+
+// Premitive
+// Number
+// String
+// Boolean
+// BigInt
+// null
+// undefined
+// Symbol
+
+
+// Non-Premitive
+// Array
+// objects
+// funtions
+
+let array = ["satrohan","sharma","from","kantibazar"];
+console.log(typeof array);
+
+// ******************************************************
+
+// Types of memory 
+//   Stack and Heap
