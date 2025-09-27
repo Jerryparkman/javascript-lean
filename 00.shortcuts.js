353a0173b3c692009d88a1a9a04f8c268f1ddecd
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-27 00:40:38 +0000

    some shortcuts

--- 00.shortcuts.js
+++ 00.shortcuts.js
@@ -1,7 +1,12 @@
-console.log("HELLO".charAt(3));
-"hello".charAt(3);
+// console.log("HELLO".charAt(3));
+// "hello".charAt(3);
 
 // "".toUpperCase(). IT used for making anything in upper letter
 // "".toLowerCase(). It used for making anything in lower letter
 // "hello".chatAt(3). To find the position of the string Eg. it gives the output (L)
-
+// `` Used for concat things Eg: "My name is ${(variable name)} sharma"
+// substring() It is used to find certain part of the string. Eg: console.log(name.substring(0,4))
+    
+// IndexOf
+// let school = "NKSS";
+// console.log(school.indexOf("S"));
