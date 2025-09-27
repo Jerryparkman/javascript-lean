24cc9a4285fe411a091489fcfe54705053208e83
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-27 00:42:14 +0000

    String methods

---
+++ 05_String.js
@@ -0,0 +1,18 @@
+// `` <= string interpolation
+// It is used for making placeholders
+
+
+
+// Method of concatination of two string together
+// This method is know as string interpolationf
+let name = "Satrohan";
+let age = 20;
+
+// console.log(`my name is ${name} and my age is ${age} and i'm currently working remotly from my home`);
+
+let newname = name.substring(0,3);
+// console.log(newname);
+
+
+let MyHobby = "Hello Buddy it is my hobby you know"
+console.log(MyHobby.substring(0, 10));
