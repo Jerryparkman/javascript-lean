d40214607e548657463d3ad83ac8d18ca92cd1c4
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-30 08:16:56 +0000

    first

---
+++ 09_array02.js
@@ -0,0 +1,22 @@
+// Combining methods of Array 
+// push()
+// concat()
+// spread()
+// copywithin()
+
+const A1 = [1,2,3,4,5];
+const A2 = [6,7,8,9,10];
+
+// Push()
+const P1 = A1.push(A2);
+// console.log(P1);
+
+// Push()
+const C1 = A1.concat(A2);
+// console.log(C1);
+
+// copywithin()
+const C2 = A2.copyWithin(0,1);
+console.log(C2);
+
+
