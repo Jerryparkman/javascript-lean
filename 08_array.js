06ffdd522321b3757c1db4938111b86fbe0f7938
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-30 01:58:15 +0000

    arrayfirst

---
+++ 08_array.js
@@ -0,0 +1,26 @@
+let myArr = ['a','b','c','d'];
+// console.log(myArr);
+
+// console.log(myArr[0]);
+// console.log(myArr.length);
+
+
+// For adding element into existing array
+myArr.push('b');
+// console.log(myArr);
+
+// For removing last added element from the existing array
+myArr.pop(0);
+// console.log(myArr);
+
+
+myArr.unshift('9')
+// console.log(myArr);
+
+myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes('a'));
+
+console.log(myArr.indexOf('a'));
+console.log(myArr.find);
