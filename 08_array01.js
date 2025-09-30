2031acd2a9f84fb185735d02d30b4c16b023b042
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-30 02:55:21 +0000

    rename

---
+++ 08_array01.js
@@ -0,0 +1,53 @@
+// ***********Array Methods***************
+// includes() 
+// push()
+// pop()
+// unshift()
+// shift();
+// indexOf();
+// length;
+// join()
+// slice
+// splice
+
+
+
+let myArr = ['a','b','c','d'];
+// console.log(myArr);
+
+// console.log(myArr[0]);
+// console.log(myArr.length);
+
+
+// For adding element into existing array
+myArr.push('b');
+// console.log(myArr);
+
+// For removing last added element from the existing array
+myArr.pop(0);
+// console.log(myArr);
+
+
+myArr.unshift('9')
+// console.log(myArr);
+
+myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes('a'));
+
+// console.log(myArr.indexOf('a'));
+
+// let newArr = myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A",myArr, "Normal array");
+const myn1 = myArr.slice(1,4);
+
+console.log("B",myn1,"Slice array");
+
+const myn2 = myArr.splice(1,4);
+console.log("C",myn2,"Splice array");
