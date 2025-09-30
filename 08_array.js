7ad405dcea7b5187fd649ba304ffd7e3aa26918f
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-30 02:53:58 +0000

    array second

--- 08_array.js
+++ 08_array.js
@@ -1,3 +1,17 @@
+// ***********Array Methods***************
+// includes() 
+// push()
+// pop()
+// unshift()
+// shift();
+// indexOf();
+// length;
+// join()
+// slice
+// splice
+
+
+
 let myArr = ['a','b','c','d'];
 // console.log(myArr);
 
@@ -22,5 +36,18 @@ myArr.shift();
 
 // console.log(myArr.includes('a'));
 
-console.log(myArr.indexOf('a'));
-console.log(myArr.find);
+// console.log(myArr.indexOf('a'));
+
+// let newArr = myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A",myArr, "Normal array");
+const myn1 = myArr.slice(1,4);
+
+console.log("B",myn1,"Slice array");
+
+const myn2 = myArr.splice(1,4);
+console.log("C",myn2,"Splice array");
