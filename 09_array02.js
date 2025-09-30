3546bdd397be61bfc2a349500a7252006f5ef09b
Jerryparkman <123185935+Jerryparkman@users.noreply.github.com>
2025-09-30 08:31:33 +0000

    second

--- 09_array02.js
+++ 09_array02.js
@@ -7,16 +7,17 @@
 const A1 = [1,2,3,4,5];
 const A2 = [6,7,8,9,10];
 
-// Push()
-const P1 = A1.push(A2);
-// console.log(P1);
+// // Push()
+// const P1 = A1.push(A2);
+// // console.log(P1);
 
-// Push()
-const C1 = A1.concat(A2);
-// console.log(C1);
+// // Push()
+// const C1 = A1.concat(A2);
+// // console.log(C1);
 
-// copywithin()
-const C2 = A2.copyWithin(0,1);
-console.log(C2);
+// // copywithin()
+// const C2 = A2.copyWithin(0,1);
+// // console.log(C2);
 
-
+const All = [...A1,...A2];
+console.log(All);
