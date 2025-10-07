33e85c06fedec051a1cfa72ea337a87474054114
jerry park <https://github.com/Jerryparkman/Project.git>
2025-10-07 08:38:19 +0545

    Final

---
+++ 17_ControlFlow.js
@@ -0,0 +1,120 @@
+// if
+// if else
+// if else if
+// Switch
+// &&
+// ||
+
+// const Money = 100;
+
+// if (Money>500){
+//     console.log("Went to janakpur");
+// }
+// else if(Money >400) {
+//     console.log("Went to bardibas!");
+// }
+// else if(Money > 300){
+//     console.log("Went to gaushala!");
+// }
+// else if(Money>200){
+//     console.log("Went to kantibazar");
+// }
+// else if(Money>=100){
+// console.log("Only went to panipuri shop bro!");
+// }
+// else{
+//     console.log("Not went to janakpur");
+// }
+
+
+// Switch statement
+// const Month = 5;
+
+// switch(Month){
+// case 1:
+// console.log("its January");
+// break;
+
+// case 2:
+// console.log("its February");
+// break;
+
+// case 3:
+//     console.log("Its March");
+//     break;
+
+// case 4:
+//     console.log("Its April");
+//     break;
+
+// case 5:
+//     console.log("its may and its my birthday bro");
+//     break;
+
+// case 6:
+//     console.log("its june");
+//     break;
+
+// case 7:
+//     console.log("its july");
+//     break;
+
+// case 8:
+//     console.log("its august");
+//     break;
+
+// case 9: 
+// console.log("september");
+// break;
+
+// case 10:
+//     console.log("its Octomber");
+//     break;
+
+// case 11:
+//     console.log("its November");
+//     break;
+
+// case 12:
+//         console.log("its december");
+//         break;
+
+//         default: 
+//         console.log("Bsdk thik se month likh k aaja");
+//         break;
+// };
+
+// // checking array is empty or not 
+// const Arr = [1,2];
+
+// if(Arr.length ===0){
+//     console.log("Array is empty");
+// }
+// else{
+//     console.log("array is not empty");
+// }
+
+// // checking object is empty or not 
+// const Obj = {Name: "Satrohan"}
+
+// if(Object.keys(Obj).length===0){
+//     console.log("Object is empty");
+// }
+// else {
+//     console.log("The object is not empty");
+// }
+
+// Falsy values
+// false, 0, -0, '', null, undefined, NaN
+
+// Truthly values
+// true, " ", [], {}, function(){}
+
+let i;
+let j;
+for(i=0;i<5;i++){
+    if(i==3){
+        continue
+    }
+    console.log(i)
+};
---
+++ 18_Loops.js
@@ -0,0 +1,15 @@
+// For loop
+// break statement
+// continue statement
+
+// for loop example with break and continue
+let i=0;
+for(i;i<6;i++){
+    if(i==3){
+        continue;
+    }
+    else if(i==5){
+        break
+    }
+    console.log(i);
+};
